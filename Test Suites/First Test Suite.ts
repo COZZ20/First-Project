eb2ac7fac16cb7d809a6ce00ce306c2fd80e296f
Castello <Castello@LAPTOP-J6Q7F4S4>
2025-04-08 12:08:40 +0800

    first test suite

---
+++ Test Suites/First Test Suite.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Browsing through McDonald's website</description>
+   <name>First Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3d4885dd-5343-4d31-99ac-97b204ad8afc</testSuiteGuid>
+   <testCaseLink>
+      <guid>c90e60a5-60a0-439b-85e8-21656182d486</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/First-Test</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
