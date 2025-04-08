7dd3fa71856850b0aa72284c7ab4d827b42129ac
Castello <Castello@LAPTOP-J6Q7F4S4>
2025-04-08 12:01:34 +0800

    Updated first test

--- Scripts/First-Test/Script1744084475475.groovy
+++ Scripts/First-Test/Script1744084475475.groovy
@@ -17,3 +17,29 @@ import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
 import org.openqa.selenium.Keys as Keys
 
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://www.mcdonalds.com.my/')
+
+WebUI.click(findTestObject('Object Repository/Page_McDonalds Malaysia/a_Our Food'))
+
+WebUI.click(findTestObject('Object Repository/Page_McDonalds Malaysia  Menu/img_Breakfast_w-full lgpx-2 food-slider__it_f3e738'))
+
+WebUI.click(findTestObject('Object Repository/Page_Sausage McMuffin  Im lovin it McDonald_8aa189/img__w-full lgpx-2 food-slider__item-image'))
+
+WebUI.click(findTestObject('Object Repository/Page_Big Breakfast  Im lovin it McDonalds Malaysia/img__w-full lgpx-2 food-slider__item-image'))
+
+WebUI.click(findTestObject('Object Repository/Page_Hash Browns  Im lovin it McDonalds Malaysia/a_Promotions'))
+
+WebUI.click(findTestObject('Object Repository/Page_McDonalds Malaysia  Promotions/img_Be Rewarded this Raya with McD App Spen_f4cdd5'))
+
+WebUI.click(findTestObject('Object Repository/Page_McDonalds Malaysia  Try the NEW Ayam T_1e3106/a_Drive-Thru'))
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_McDonalds Malaysia  Locate Us/select_All Johor Kedah Kelantan Kuala Lumpu_383a45'), 
+    'Melaka', true)
+
+WebUI.selectOptionByValue(findTestObject('Object Repository/Page_McDonalds Malaysia  Locate Us/select_All Categories 24 Hours Birthday Par_25a78b'), 
+    '8', true)
+
+WebUI.closeBrowser()
+
